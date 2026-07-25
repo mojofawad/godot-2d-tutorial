6525afdb0bb5711afaef9a213824927ae79342fe
mojofawad <mojofawad@gmail.com>
2026-07-25 18:05:17 -0400

    wire up inputs to move player

--- Player.cs
+++ Player.cs
@@ -13,9 +13,44 @@ public partial class Player : Area2D
     {
         ScreenSize = GetViewportRect().Size;
     }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    
     public override void _Process(double delta)
     {
+        var velocity = Vector2.Zero;
+
+        if (Input.IsActionPressed("move_up"))
+        {
+            velocity.Y -= 1;
+        }
+        if (Input.IsActionPressed("move_down"))
+        {
+            velocity.Y += 1;
+        }
+        if (Input.IsActionPressed("move_left"))
+        {
+            velocity.X -= 1;
+        }
+        if (Input.IsActionPressed("move_right"))
+        {
+            velocity.X += 1;
+        }
+        
+        var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * Speed;
+            animatedSprite2D.Play();
+        }
+        else
+        {
+            animatedSprite2D.Stop();
+        }
+
+        Position += velocity * (float)delta;
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+            y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+        );
     }
 }
